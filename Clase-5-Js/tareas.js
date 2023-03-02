8cec25230a2d6d8c970e4b3ba77ac17fd595ef06
Mati <matias.portillo996@gmail.com>
2023-03-01 22:47:04 -0300

    Tarea clase 5

--- Clase-5-Js/tareas.js
+++ Clase-5-Js/tareas.js
@@ -7,7 +7,17 @@ function counter() {
   // ejemplo: const newCounter = counter();
   // newCounter(); // 1
   // newCounter(); // 2
+  let counter = 0;
+
+  return function () {
+    return ++counter;
+  }
 }
+
+newCounter = counter ();
+newCounter();
+newCounter();
+newCounter();
 
 function cacheFunction(cb) {
   // Usa closures para crear un caché para la función cb.
@@ -21,7 +31,20 @@ function cacheFunction(cb) {
   // si la invocas de nuevo con 5, deberia retornar 25 (guardado previament en el cache)
   // Tips, usá un objeto donde cada propiedad sea un argumento, y el valor el resultado.
   // usá hasOwnProperty!
+  let cache = {}
+  return function (argumento) {
+    if(cache.hasOwnProperty(argumento)) {
+      return cache[argumento];
+    }else{
+      cache[arguemento] = cb (argumento);
+      return cache[argumento];
+    }
+  }
 }
+const nuevoCache = cacheFunction(5);
+nuevoCache(5);
+nuevoCache(20);
+nuevoCache(25);
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
